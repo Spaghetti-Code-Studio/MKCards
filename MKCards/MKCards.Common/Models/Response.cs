efef3c16aefcaeaaac9b31c87bbe18e13eda3e81
Marek Eibel <536376@mail.muni.cz>
2025-07-10 17:40:18 +0200

    feat: devide common models into seperated files

---
+++ MKCards/MKCards.Common/Models/Response.cs
@@ -0,0 +1,51 @@
+﻿namespace MKCards.Common.Models
+{
+	public class CreateGameRequest
+	{
+		public string GameName { get; set; } = string.Empty;
+		public string PlayerName { get; set; } = string.Empty;
+		public int MaxPlayers { get; set; } = 4;
+	}
+
+	public class JoinGameRequest
+	{
+		public string GameId { get; set; } = string.Empty;
+		public string PlayerName { get; set; } = string.Empty;
+	}
+
+	public class PlayerJoinedResponse
+	{
+		public bool Success { get; set; }
+		public string PlayerId { get; set; } = string.Empty;
+		public string PlayerName { get; set; } = string.Empty;
+		public GameServer? GameServer { get; set; }
+	}
+
+	public class PlayerLeftResponse
+	{
+		public bool Success { get; set; }
+		public GameServer? GameServer { get; set; }
+	}
+
+	public class GameServerResponse
+	{
+		public bool Success { get; set; }
+		public string Message { get; set; } = string.Empty;
+		public string GameId { get; set; } = string.Empty;
+		public GameServer? GameServer { get; set; }
+	}
+
+	public class MessageSentResponse
+	{
+		public bool Success { get; set; }
+		public string PlayerName { get; set; } = string.Empty;
+		public string Message { get; set; } = string.Empty;
+		public DateTime TimeStamp { get; set; }
+	}
+
+	public class GameStartedResponse
+	{
+		public bool Success { get; set; }
+		public GameServer? GameServer { get; set; }
+	}
+}
