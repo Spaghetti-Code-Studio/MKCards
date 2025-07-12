e8bdb49aa9625e7d8f6cd121169a98826854f1e0
Marek Eibel <536376@mail.muni.cz>
2025-07-12 21:57:33 +0200

    feat: add builder for EF db context + implementation for pomelo EF mysql
    the idea is that users of MKCards library should be able to connect to their db systems via possibly different EF favor => different options available for them, thus different IDbContextOptionsBuilder impl

---
+++ MKCards/MKCards.Server/Data/MariaDbContextOptionsBuilder.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MKCards.Server.Data
+{
+	public class MariaDbContextOptionsBuilder : IDbContextOptionsBuilder
+	{
+		string ConnectionString { get; set; } = string.Empty;
+		Version ServerVersion { get; set; } = new Version(1, 1, 1);
+
+		/// <summary>
+		/// Enables certain functionalies such as logging of sensitive data.
+		/// Never enable this property in production unless you're debugging a critical issue and understand the consequences.
+		/// May leak passwords, emails, or private info into logs.
+		/// Allow it only when developing.
+		/// </summary>
+		bool DebugMode { get; set; } = false;
+
+		public MariaDbContextOptionsBuilder(string connectionString, Version serverVersion, bool debugMode = false)
+		{
+			ConnectionString = connectionString;
+			ServerVersion = serverVersion;
+			DebugMode = debugMode;
+		}
+
+		// TODO: we might need to allow better logging in the future via e.g. LogTo(Console.WriteLine, LogLevel.Information). The question is where to log. Client actually includes Logger, but Server does not.
+		public Action<DbContextOptionsBuilder> GetConfiguration()
+		{
+			return options =>
+			{
+				options.UseMySql(ConnectionString, new MySqlServerVersion(ServerVersion));
+				if (DebugMode)
+				{
+					options.EnableSensitiveDataLogging();
+					options.EnableDetailedErrors();
+				}
+			};
+		}
+	}
+}
--- MKCards/MKCards.Server/Program.cs
+++ MKCards/MKCards.Server/Program.cs
@@ -10,75 +10,74 @@ using MKCards.Server.Components.Account;
 
 namespace MKCards.Server
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddRazorComponents()
-                .AddInteractiveServerComponents()
-                .AddInteractiveWebAssemblyComponents();
+			builder.Services.AddRazorComponents()
+				.AddInteractiveServerComponents()
+				.AddInteractiveWebAssemblyComponents();
 
-            builder.Services.AddSignalR();
-            builder.Services.AddSingleton<IGameServerService, GameServerService>();
+			builder.Services.AddSignalR();
+			builder.Services.AddSingleton<IGameServerService, GameServerService>();
 
-            builder.Services.AddCascadingAuthenticationState();
-            builder.Services.AddScoped<IdentityUserAccessor>();
-            builder.Services.AddScoped<IdentityRedirectManager>();
-            builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
+			builder.Services.AddCascadingAuthenticationState();
+			builder.Services.AddScoped<IdentityUserAccessor>();
+			builder.Services.AddScoped<IdentityRedirectManager>();
+			builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
 
-            builder.Services.AddAuthentication(options =>
-                {
-                    options.DefaultScheme = IdentityConstants.ApplicationScheme;
-                    options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-                })
-                .AddIdentityCookies();
+			builder.Services.AddAuthentication(options =>
+				{
+					options.DefaultScheme = IdentityConstants.ApplicationScheme;
+					options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+				})
+				.AddIdentityCookies();
 
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+			var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+			IDbContextOptionsBuilder dbContextOptionsBuilder = new MariaDbContextOptionsBuilder(connectionString, new Version(11, 8, 2));
+			builder.Services.AddDbContext<ApplicationDbContext>(dbContextOptionsBuilder.GetConfiguration());
+			builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddSignInManager()
-                .AddDefaultTokenProviders();
+			builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+				.AddEntityFrameworkStores<ApplicationDbContext>()
+				.AddSignInManager()
+				.AddDefaultTokenProviders();
 
-            builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+			builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 
-            var app = builder.Build();
+			var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseWebAssemblyDebugging();
-                app.UseMigrationsEndPoint();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+			// Configure the HTTP request pipeline.
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseWebAssemblyDebugging();
+				app.UseMigrationsEndPoint();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
 
-            app.UseHttpsRedirection();
+			app.UseHttpsRedirection();
 
-            app.UseStaticFiles();
-            app.UseAntiforgery();
+			app.UseStaticFiles();
+			app.UseAntiforgery();
 
-            app.MapRazorComponents<App>()
-                .AddInteractiveServerRenderMode()
-                .AddInteractiveWebAssemblyRenderMode()
-                .AddAdditionalAssemblies(typeof(Client._Imports).Assembly);
+			app.MapRazorComponents<App>()
+				.AddInteractiveServerRenderMode()
+				.AddInteractiveWebAssemblyRenderMode()
+				.AddAdditionalAssemblies(typeof(Client._Imports).Assembly);
 
-            // Add additional endpoints required by the Identity /Account Razor components.
-            app.MapAdditionalIdentityEndpoints();
+			// Add additional endpoints required by the Identity /Account Razor components.
+			app.MapAdditionalIdentityEndpoints();
 
-            app.MapHub<GameHub>("/gamehub");
+			app.MapHub<GameHub>("/gamehub");
 
-            app.Run();
-        }
-    }
+			app.Run();
+		}
+	}
 }
